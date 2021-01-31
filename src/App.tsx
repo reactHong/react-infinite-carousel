0b89fab55c2518fcce9ac9040dd511a55d1b63f9
Hong Shik Kim <hong@hongs.dev>
2021-01-31 17:17:27 +0900

    fix: Fix a compile error because of testData location

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Discovery, { Section } from './components/Discovery';
-import { realDiscoveryData, testDiscoveryData } from './testData';
 
 type DiscoveryData = {
   sections: Section[];
@@ -12,19 +11,17 @@ function App() {
   const [discoveryData, setDiscoveryData] = useState<DiscoveryData>({
     sections: [],
   });
-  // const [discoveryData, setDiscoveryData] = useState<DiscoveryData>(realDiscoveryData);
 
   useEffect(() => {
     fetch("./discovery_page.json")
       .then(response => response.json())
       .then(data => {
-        console.log("[App.componentDidMount] data:", data);
         setDiscoveryData(data);
       })
       .catch(err => {
-        console.log("[App.componentDidMount] Error:", err);
+        //TODO: Direct to the error page
+        alert(err);
       });
-
   }, []);
 
   return (
